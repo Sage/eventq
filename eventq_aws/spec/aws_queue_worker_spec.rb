66aa1c12607938090b1a85786c48508972aeddf3
Vaughan Britton <vaughan.britton@sage.com>
2016-07-01 21:00:43 +0100

    Update aws_queue_worker_spec.rb

--- eventq_aws/spec/aws_queue_worker_spec.rb
+++ eventq_aws/spec/aws_queue_worker_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 RSpec.describe EventQ::Amazon::QueueWorker do
 
   let(:queue_client) do
-    EventQ::Amazon::QueueClient.new({ aws_account_number: 997524026223 })
+    EventQ::Amazon::QueueClient.new({ aws_account_number: '{account number goes here}' })
   end
 
   let(:queue_manager) do
