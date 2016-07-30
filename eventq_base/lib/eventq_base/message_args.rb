3a0c273845a8ea0b2d6a15381ede4543e4004ad2
vaughanbrittonsage <vaughan.britton@sage.com>
2016-07-30 10:13:20 +0100

    added drop attribute to message args

--- eventq_base/lib/eventq_base/message_args.rb
+++ eventq_base/lib/eventq_base/message_args.rb
@@ -3,11 +3,13 @@ module EventQ
     attr_reader :type
     attr_reader :retry_attempts
     attr_accessor :abort
+    attr_accessor :drop
 
     def initialize(type, retry_attempts)
       @type = type
       @retry_attempts = retry_attempts
       @abort = false
+      @drop = false
     end
   end
 end
--- eventq_base/lib/eventq_base/version.rb
+++ eventq_base/lib/eventq_base/version.rb
@@ -1,3 +1,3 @@
 module EventqBase
-  VERSION = "1.3.1"
+  VERSION = "1.4.0"
 end
