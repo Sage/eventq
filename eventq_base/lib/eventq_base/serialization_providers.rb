f5daf6bff24090512586c538e6c5f4d4901464ed
vaughanbrittonsage <vaughan.britton@sage.com>
2016-09-04 11:24:35 +0100

    added serialization provider manager

--- eventq_base/lib/eventq_base/serialization_providers.rb
+++ eventq_base/lib/eventq_base/serialization_providers.rb
@@ -3,7 +3,24 @@ require_relative 'serialization_providers/oj_serialization_provider'
 
 module EventQ
   module SerializationProviders
+
     OJ = 'oj'.freeze
     JSON = 'json'.freeze
+
+    class Manager
+      def initialize
+        @providers = {}
+        @providers[OJ] = EventQ::SerializationProviders::OjSerializationProvider.new
+        @providers[JSON] = EventQ::SerializationProviders::JsonSerializationProvider.new
+      end
+
+      def get_provider(provider_type)
+        provider = @providers[provider_type]
+        if provider == nil
+          raise "Invalid provider type spedcified: #{provider_type}"
+        end
+        return provider
+      end
+    end
   end
 end
