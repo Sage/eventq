987c401c070038e2794c11d44d1a03cf9bdc5d31
vaughanbrittonsage <vaughan.britton@sage.com>
2016-10-24 13:15:32 +0100

    updated oj serialization provider to explicitly use :object mode for Oj.dump

--- eventq_base/lib/eventq_base/serialization_providers/oj_serialization_provider.rb
+++ eventq_base/lib/eventq_base/serialization_providers/oj_serialization_provider.rb
@@ -7,13 +7,14 @@ module EventQ
       end
 
       def serialize(object)
-        return Oj.dump(object)
+        return Oj.dump(object, mode: :object)
       end
 
       def deserialize(json)
         begin
           return Oj.load(json)
         rescue Oj::ParseError
+          EventQ.log(:debug, "[#{self.class}] - Failed to deserialize using Oj, falling back to JsonSerializationProvider.")
           return @json_serializer.deserialize(json)
         end
       end
