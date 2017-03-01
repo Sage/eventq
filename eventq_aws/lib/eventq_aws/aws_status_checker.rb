fe561ab9ed2c6a59eae394e199c1d085fd51565b
vaughanbrittonsage <vaughan.britton@sage.com>
2017-03-01 12:31:58 +0000

    added status checker to aws provider

---
+++ eventq_aws/lib/eventq_aws/aws_status_checker.rb
@@ -0,0 +1,25 @@
+module EventQ
+  module Amazon
+    class StatusChecker
+
+      def initialize(queue_manager)
+
+        if queue_manager == nil
+          raise 'queue_manager  must be specified.'.freeze
+        end
+
+        @queue_manager = queue_manager
+
+      end
+
+      def queue?(queue)
+        @queue_manager.queue_exists?(queue)
+      end
+
+      def event_type?(event_type)
+        @queue_manager.topic_exists?(event_type)
+      end
+
+    end
+  end
+end
