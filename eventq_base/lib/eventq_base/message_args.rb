f952af06ddaf0c85edd6820ff300e193b907005c
vaughanbrittonsage <vaughan.britton@sage.com>
2017-06-14 09:41:49 +0100

    updated docker image for testing and added context to queue_message and message_args

--- eventq_base/lib/eventq_base/message_args.rb
+++ eventq_base/lib/eventq_base/message_args.rb
@@ -4,12 +4,14 @@ module EventQ
     attr_reader :retry_attempts
     attr_accessor :abort
     attr_accessor :drop
+    attr_reader :context
 
-    def initialize(type, retry_attempts)
+    def initialize(type, retry_attempts, context = {})
       @type = type
       @retry_attempts = retry_attempts
       @abort = false
       @drop = false
+      @context = context
     end
   end
 end
--- eventq_base/lib/eventq_base/queue_message.rb
+++ eventq_base/lib/eventq_base/queue_message.rb
@@ -8,11 +8,13 @@ module EventQ
     attr_accessor_type :content
     attr_accessor_type :created, type: Time
     attr_accessor_type :signature, type: String
+    attr_accessor_type :context, type: Hash
 
     def initialize
       @retry_attempts = 0
       @created = Time.now.to_i
       @id = SecureRandom.uuid
+      @context = {}
     end
 
     # Creates a signature for the message
