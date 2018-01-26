e1938df2d417f817ca54f85a9ce85079e41897ce
vaughanbrittonsage <vaughan.britton@sage.com>
2018-01-26 11:51:53 +0000

    bugfix for DateTime attribute writer

--- eventq_base/lib/eventq_base/serialization_providers/jruby/oj/date_time_writer.rb
+++ eventq_base/lib/eventq_base/serialization_providers/jruby/oj/date_time_writer.rb
@@ -7,6 +7,13 @@ module EventQ
             obj.is_a?(DateTime)
           end
           def exec(obj)
+            seconds = obj.strftime('%S%N')
+            d = 1_000_000_000
+            if seconds.start_with?('0')
+              seconds[0] = ''
+              d = 100_000_000
+            end
+
             {
               '^O': 'DateTime',
               year: obj.year,
@@ -14,7 +21,7 @@ module EventQ
               day: obj.day,
               hour: obj.hour,
               min: obj.min,
-              sec: RationalWriter.new.exec(Rational(Integer(obj.strftime('%S%N')), 1000000000)),
+              sec: RationalWriter.new.exec(Rational(Integer(seconds), d)),
               offset: RationalWriter.new.exec(obj.offset),
               start: obj.start
             }
