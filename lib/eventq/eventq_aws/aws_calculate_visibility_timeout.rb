ee25c0808a86a32d587b926bf79a0361e1997124
Victor Martins <victor.martins@sage.com>
2018-11-30 12:05:29 +0000

    refactoring

--- lib/eventq/eventq_aws/aws_calculate_visibility_timeout.rb
+++ lib/eventq/eventq_aws/aws_calculate_visibility_timeout.rb
@@ -8,24 +8,38 @@ module EventQ
       end
 
       def call(retry_attempts:, retry_delay:, retry_back_off_grace:, max_retry_delay:, allow_retry_back_off:)
-        retry_attempts = retry_attempts - retry_back_off_grace
-        retry_attempts = 1 if retry_attempts < 1
+        retry_attempts = apply_back_off_grace(retry_attempts, retry_back_off_grace)
 
-        if allow_retry_back_off
-          visibility_timeout = (retry_delay * retry_attempts) / 1000
-          if visibility_timeout > (max_retry_delay / 1000)
-            EventQ.logger.debug { "[#{self.class}] - Max message back off retry delay reached." }
-            visibility_timeout = max_retry_delay / 1000
-          end
-        else
-          visibility_timeout = retry_delay / 1000
-        end
+        return timeout_without_back_off(retry_delay) unless allow_retry_back_off
+
+        visibility_timeout = timeout_with_back_off(retry_delay, retry_attempts, max_retry_delay)
 
         if visibility_timeout > @max_timeout
           EventQ.logger.debug { "[#{self.class}] - AWS max visibility timeout of 12 hours has been exceeded. Setting message retry delay to 12 hours." }
           visibility_timeout = @max_timeout
         end
 
+        visibility_timeout
+      end
+
+      private
+
+      def timeout_without_back_off(retry_delay)
+        retry_delay / 1000
+      end
+
+      def apply_back_off_grace(retry_attempts, retry_back_off_grace)
+        retry_attempts = retry_attempts - retry_back_off_grace
+        retry_attempts = 1 if retry_attempts < 1
+        retry_attempts
+      end
+
+      def timeout_with_back_off(retry_delay, retry_attempts, max_retry_delay)
+        visibility_timeout = (retry_delay * retry_attempts) / 1000
+        if visibility_timeout > (max_retry_delay / 1000)
+          EventQ.logger.debug { "[#{self.class}] - Max message back off retry delay reached." }
+          visibility_timeout = max_retry_delay / 1000
+        end
         visibility_timeout
       end
     end
