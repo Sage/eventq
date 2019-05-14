64440c44d81f6e8e6c25dc166d95674cd4ba68ac
Rolf Wadstein <rolf.wadstein@sage.com>
2019-05-14 11:43:43 -0400

    raise sns topic not found error

--- lib/eventq/eventq_aws/aws_subscription_manager.rb
+++ lib/eventq/eventq_aws/aws_subscription_manager.rb
@@ -20,17 +20,13 @@ module EventQ
         end
 
         topic_arn = @client.sns_helper(topic_region).public_send(method, event_type, topic_region)
+        raise Exceptions::EventTypeNotFound, 'SNS topic not found, unable to subscribe' unless topic_arn
 
         q = @manager.get_queue(queue)
         queue_arn = @client.sqs_helper(queue_region).get_queue_arn(queue)
 
-        @client.sqs(queue_region).set_queue_attributes(
-          queue_url: q,
-          attributes:
-            {
-              'Policy' => queue_policy(queue_arn)
-            }
-        )
+        attributes = default_queue_attributes(q, queue_arn)
+        @client.sqs(queue_region).set_queue_attributes(attributes)
 
         @client.sns(topic_region).subscribe(
           topic_arn: topic_arn,
@@ -47,6 +43,18 @@ module EventQ
 
       def unsubscribe(_queue)
         raise "[#{self.class}] - Not implemented. Please unsubscribe the queue from the topic inside the AWS Management Console."
+      end
+
+      private
+
+      def default_queue_attributes(queue, queue_arn)
+        {
+          queue_url: queue,
+          attributes:
+            {
+              'Policy' => queue_policy(queue_arn)
+            }
+        }
       end
 
       def queue_policy(queue_arn)
