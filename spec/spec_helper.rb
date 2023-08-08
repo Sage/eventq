9f16555865deb5fdaa31f310b71bd7cb67cf98d2
Ian Oxley <ian.oxley@sage.com>
2023-08-08 14:40:16 +0100

    Remove empty before each block

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,11 +20,6 @@ require_relative '../lib/eventq/rabbitmq'
 require_relative '../lib/eventq/aws'
 
 RSpec.configure do |config|
-
-  config.before(:each) do
-
-  end
-
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
