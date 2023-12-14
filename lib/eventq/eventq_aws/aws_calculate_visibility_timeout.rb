f90145205efc51610c1eb9d262330c3c43a5153c
Florian Pilz <florian.pilz@sage.com>
2023-12-14 15:35:46 +0100

    Refactor aws_calculate_visibility_timeout
    * Extract check for max retry delay to its own method
    * Use milliseconds throughout the class and only convert to seconds when
      returning the final visibility timeout
    * Use guard clauses in the check_... methods
    * Ensure adhering to limits inside the timeout method

--- lib/eventq/eventq_aws/aws_calculate_visibility_timeout.rb
+++ lib/eventq/eventq_aws/aws_calculate_visibility_timeout.rb
@@ -5,7 +5,7 @@ module EventQ
     # Class responsible to know how to calculate message Visibility Timeout for Amazon SQS
     class CalculateVisibilityTimeout
       def initialize(max_timeout:, logger: EventQ.logger)
-        @max_timeout = max_timeout
+        @max_timeout = seconds_to_ms(max_timeout)
         @logger      = logger
       end
 
@@ -30,14 +30,13 @@ module EventQ
         @retry_back_off_weight      = queue_settings.fetch(:retry_back_off_weight)
         @retry_delay                = queue_settings.fetch(:retry_delay)
 
-        if @allow_retry_back_off && retry_past_grace_period?
-          visibility_timeout = timeout_with_back_off
-          visibility_timeout = check_for_max_timeout(visibility_timeout)
+        visibility_timeout = if @allow_retry_back_off && retry_past_grace_period?
+          timeout_with_back_off
         else
-          visibility_timeout = timeout_without_back_off
+          timeout_without_back_off
         end
 
-        visibility_timeout
+        ms_to_seconds(visibility_timeout)
       end
 
       private
@@ -49,38 +48,50 @@ module EventQ
       end
 
       def timeout_without_back_off
-        ms_to_seconds(@retry_delay)
+        @retry_delay
       end
 
       def timeout_with_back_off
         factor = @retry_attempts - @retry_back_off_grace
+        weighted_retry_delay = @retry_delay * @retry_back_off_weight
 
         visibility_timeout = if @allow_exponential_back_off
-          ms_to_seconds(@retry_delay * @retry_back_off_weight * 2 ** (factor - 1))
+          weighted_retry_delay * 2 ** (factor - 1)
         else
-          ms_to_seconds(@retry_delay * @retry_back_off_weight * factor)
+          weighted_retry_delay * factor
         end
 
-        max_retry_delay = ms_to_seconds(@max_retry_delay)
-
-        if visibility_timeout > max_retry_delay
-          logger.debug { "[#{self.class}] - Max message back off retry delay reached: #{max_retry_delay}" }
-          visibility_timeout = max_retry_delay
-        end
-
-        visibility_timeout
+        visibility_timeout = check_for_max_retry_delay(visibility_timeout)
+        check_for_max_timeout(visibility_timeout)
       end
 
       def ms_to_seconds(value)
         value / 1000
       end
 
-      def check_for_max_timeout(visibility_timeout)
-        if visibility_timeout > @max_timeout
-          logger.debug { "[#{self.class}] - AWS max visibility timeout of 12 hours has been exceeded. Setting message retry delay to 12 hours." }
-          visibility_timeout = @max_timeout
+      def seconds_to_ms(value)
+        value * 1000
+      end
+
+      def check_for_max_retry_delay(visibility_timeout)
+        return visibility_timeout if visibility_timeout <= @max_retry_delay
+
+        logger.debug do
+          "[#{self.class}] - Max message back off retry delay reached: #{ms_to_seconds(@max_retry_delay)}"
         end
-        visibility_timeout
+
+        @max_retry_delay
+      end
+
+      def check_for_max_timeout(visibility_timeout)
+        return visibility_timeout if visibility_timeout <= @max_timeout
+
+        logger.debug do
+          "[#{self.class}] - AWS max visibility timeout of 12 hours has been exceeded. "\
+          "Setting message retry delay to 12 hours."
+        end
+
+        @max_timeout
       end
     end
   end
